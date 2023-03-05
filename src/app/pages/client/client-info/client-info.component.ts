35bb2cf95c67f0b2281cf78114e64ddcb8a9c8f9
carloscodify <carlos.gutierrez@codify.com.bo>
2023-03-04 23:04:34 -0400

    Refactor and add filter of customer_type in list-client

--- src/app/pages/client/client-info/client-info.component.ts
+++ src/app/pages/client/client-info/client-info.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from "@angular/router";
-import {SupabaseService} from "../../supabase.service";
+import {SupabaseService} from "../../../supabase.service";
 
 
 @Component({
